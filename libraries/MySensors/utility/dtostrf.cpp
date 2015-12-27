00c68fbe54081889f7705c8ea41fafb92150e69e
Marcelo Aquino <celonunes@gmail.com>
2015-12-27 11:18:02 -0200

    update dtostrf for RPi

--- libraries/MySensors/utility/dtostrf.cpp
+++ libraries/MySensors/utility/dtostrf.cpp
@@ -1,21 +1,8 @@
 #include <stdio.h>
-#include <string.h>
 #include "dtostrf.h"
-#include "itoa.h"
 
 char *dtostrf(float f, int width, int decimals, char *result)
 {
-    char widths[3];
-    char decimalss[3];  
-    char format[100];
-    itoa(width,widths,10);
-    itoa(decimals,decimalss,10);
-    strcpy(format,"%");
-    strcat(format,widths);
-    strcat(format,".");
-    strcat(format,decimalss);
-    strcat(format,"f");
-  
-    sprintf(result,format,f);
-    return result;
+	sprintf(result,"%*.*f", width, decimals, f);
+	return result;
 }
