9d93cf5a81fbb049df56b75e1f38bda67a67ecd1
aaron832 <aaron832@gmail.com>
2016-02-15 20:27:46 -0800

    Put the ethernet socket logic back in.  This way the controller can be either ethernet (MYSController or Other) and also MQTT.  The downside of this is that MQTT duplicates data to ethernet.  Once for when we receive a C_SET from a node, and again when we receive the subscription callback for our own MQTT publish from the C_SET.

--- libraries/MySensors/examples_RPi/PiGatewayMQTT.cpp
+++ libraries/MySensors/examples_RPi/PiGatewayMQTT.cpp
@@ -27,10 +27,16 @@
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
 
 #include <MyTransportNRF24.h>
 #include <MySigningNone.h>
 #include <MySigningAtsha204Soft.h>
+#include <MyParserSerial.h>
 #include <MySensor.h>
 #include <RF24.h>
 #include <mosquitto.h>
@@ -61,6 +67,10 @@ struct radio_message {
 
 void msg_callback(const MyMessage &message);
 void *get_in_addr(struct sockaddr *sa);
+void *waiting_controllers(void *);
+void *connected_controller(void* thread_arg);
+void parse_and_send(long int sockfd, char *buffer);
+
 void *mqtt_thread(void *);
 void parsemqtt_and_send(char *topic, const char *payload);
 
@@ -73,6 +83,7 @@ struct mosquitto *mosq;
 #else
 	static MyTransportNRF24 transport(RPI_V2_GPIO_P1_22, BCM2835_SPI_CS0, RF24_PA_LEVEL_GW, BCM2835_SPI_SPEED_8MHZ);
 #endif
+static MyParserSerial parser;
 
 static std::vector<int> controllers_sockets;
 static std::list<struct radio_message> radio_messages_q;
@@ -80,10 +91,8 @@ static pthread_mutex_t controllers_sockets_mutex = PTHREAD_MUTEX_INITIALIZER;
 static pthread_mutex_t radio_messages_mutex = PTHREAD_MUTEX_INITIALIZER;
 static pthread_cond_t radio_messages_cond = PTHREAD_COND_INITIALIZER;
 
-//TODO: not sure if this should increment or what to do with it.
-static int message_id = 65;
-
 //TODO: static address for now
+//MQTT Host, Port and keepalive
 const char *mqtthost = "localhost";
 const int mqttport = 1883;
 const int mqttkeepalive = 60;
@@ -137,6 +146,7 @@ const char * subTypeToStr(int command, int type)
 int main()
 {
 	pthread_t thread_id;
+	pthread_attr_t mqtt_attr;
 	pthread_attr_t attr;
 
 	/* register the signal handler */
@@ -173,8 +183,14 @@ int main()
 	/* network thread */
 	pthread_attr_init(&attr);
 	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
-	pthread_create(&thread_id, &attr, &mqtt_thread, NULL);
+	pthread_create(&thread_id, &attr, &waiting_controllers, NULL);
 	pthread_attr_destroy(&attr);
+	
+	/* mqtt thread */
+	pthread_attr_init(&mqtt_attr);
+	pthread_attr_setdetachstate(&mqtt_attr, PTHREAD_CREATE_DETACHED);
+	pthread_create(&thread_id, &mqtt_attr, &mqtt_thread, NULL);
+	pthread_attr_destroy(&mqtt_attr);
 
 #ifndef USE_INTERRUPT
 	while(running) {
@@ -217,16 +233,31 @@ end:
 
 void msg_callback(const MyMessage &message)
 {
-	char topic[MAXTOPICSIZE];
+	char buf[MAXDATASIZE];
 	char convBuf[MAX_PAYLOAD*2+1];
 	int nbytes;
 
+	/* Ethernet */
+	printf("[CALLBACK]%d;%d;%d;%d;%d;%s\n", message.sender,
+			message.sensor, mGetCommand(message), mGetAck(message),
+			message.type, message.getString(convBuf));
+
+	nbytes = snprintf(buf, MAXDATASIZE, "%d;%d;%d;%d;%d;%s\n",
+			message.sender, message.sensor, mGetCommand(message),
+			mGetAck(message), message.type, message.getString(convBuf));
+
+	pthread_mutex_lock(&controllers_sockets_mutex);
+	for (unsigned int i = 0; i < controllers_sockets.size(); i++) {
+		if (send(controllers_sockets[i], buf, nbytes, 0) == -1)
+			perror("send");
+	}
+	pthread_mutex_unlock(&controllers_sockets_mutex);
+	
+	
+	/* MQTT */
+	char topic[MAXTOPICSIZE];
+
 	message.getString(convBuf);
-
-	//printf("[CALLBACK]%d;%d;%d;%d;%d;%s\n", message.sender,
-	//		message.sensor, mGetCommand(message), mGetAck(message),
-	//		message.type, convBuf);
-
 	nbytes = strlen(convBuf);
 
 	//Build the MQTT topic from the mysensor message.
@@ -267,6 +298,166 @@ void msg_callback(const MyMessage &message)
 	}
 }
 
+/* Ethernet */
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+	if (sa->sa_family == AF_INET) {
+		return &(((struct sockaddr_in*)sa)->sin_addr);
+	}
+
+	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+void *waiting_controllers(void *)
+{
+	// Code from:
+	//  http://beej.us/guide/bgnet/output/html/singlepage/bgnet.html#simpleserver
+
+	int sockfd;  // listen on sock_fd
+	long int new_fd; // new connection on new_fd
+	struct addrinfo hints, *servinfo, *p;
+	struct sockaddr_storage their_addr; // connector's address information
+	socklen_t sin_size;
+	int yes=1;
+	char s[INET6_ADDRSTRLEN];
+	int rv;
+	pthread_t thread_id;
+	pthread_attr_t attr;
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE; // use my IP
+
+	if ((rv = getaddrinfo(NULL, CONTROLLERS_PORT, &hints, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return NULL;
+	}
+
+	// loop through all the results and bind to the first we can
+	for (p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype,
+			p->ai_protocol)) == -1) {
+			perror("server: socket");
+			continue;
+		}
+
+		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+			sizeof(int)) == -1) {
+			perror("setsockopt");
+			exit(1);
+		}
+
+		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			close(sockfd);
+			perror("server: bind");
+			continue;
+		}
+
+		break;
+	}
+
+	freeaddrinfo(servinfo); // all done with this structure
+
+	if (p == NULL)  {
+		fprintf(stderr, "server: failed to bind\n");
+		exit(1);
+	}
+
+	if (listen(sockfd, BACKLOG) == -1) {
+		perror("listen");
+		exit(1);
+	}
+
+	printf("server: waiting for connections from controllers...\n");
+
+	while (1) {  // accept() loop
+		sin_size = sizeof their_addr;
+		new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+		if (new_fd == -1) {
+			perror("accept");
+			continue;
+		}
+
+		inet_ntop(their_addr.ss_family,
+		get_in_addr((struct sockaddr *)&their_addr),
+				s, sizeof s);
+		printf("server: got connection from %s\n", s);
+
+		pthread_attr_init(&attr);
+		pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+		pthread_create(&thread_id, &attr, &connected_controller, (void *)new_fd);
+		pthread_attr_destroy(&attr);
+
+		pthread_mutex_lock(&controllers_sockets_mutex);
+		controllers_sockets.push_back(new_fd);
+		pthread_mutex_unlock(&controllers_sockets_mutex);
+	}
+
+	return NULL;
+}
+
+void *connected_controller(void* thread_arg)
+{
+	long int sockfd = (long int) thread_arg;
+	char buf[MAXDATASIZE];
+	int nbytes;
+
+	while (1) {
+		if ((nbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) > 0) {
+			for (int i = 0; i < nbytes; i++) {
+				if (buf[i] == '\n') {
+					buf[i] = '\0';
+					parse_and_send(sockfd, buf);
+					break;
+				}
+			}
+		} else {
+			break;
+		}
+	}
+	
+	if (nbytes == -1)
+		perror("recv");
+
+	pthread_mutex_lock(&controllers_sockets_mutex);
+	auto it = std::find(controllers_sockets.begin(), controllers_sockets.end(), sockfd);
+	if (it != controllers_sockets.end()) {
+		// use swap to prevent moving all elements
+		std::swap(*it, controllers_sockets.back());
+		controllers_sockets.pop_back();
+	}
+	pthread_mutex_unlock(&controllers_sockets_mutex);
+
+	close(sockfd);
+	return NULL;
+}
+
+void parse_and_send(long int sockfd, char *buffer)
+{
+	char buf[MAXDATASIZE];
+	MyMessage msg;
+	int nbytes;
+
+	if (parser.parse(msg, buffer)) {
+		uint8_t command = mGetCommand(msg);
+
+		if (msg.destination==GATEWAY_ADDRESS && command==C_INTERNAL) {
+			// Handle messages directed to gateway
+			if (msg.type == I_VERSION) {
+				// Request for version
+				nbytes = snprintf(buf, MAXDATASIZE, "0;0;%d;0;%d;%s\n", C_INTERNAL, I_VERSION, LIBRARY_VERSION);
+				if (send(sockfd, buf, nbytes, 0) == -1)
+					perror("send");
+			}
+		} else {
+			gw->sendRoute(msg);
+		}
+	}
+}
+
+/* MQTT */
 void mqtt_message_callback(struct mosquitto *mosq, void *userdata, const struct mosquitto_message *message)
 {
 	if(message->payloadlen){
@@ -313,9 +504,6 @@ void mqtt_log_callback(struct mosquitto *mosq, void *userdata, int level, const 
 
 void *mqtt_thread(void *)
 {
-	pthread_t thread_id;
-	pthread_attr_t attr;
-
 	mosquitto_lib_init();
 	bool clean_session = true; //will clean out subscriptions  on disconnect
 	mosq = mosquitto_new(NULL, clean_session, NULL);
@@ -403,6 +591,23 @@ void parsemqtt_and_send(char *topic, const char *payload)
 	{
 		if(!gw->sendRoute(msg))
 			printf("Message not sent!\n");
+
+		// Forward the data to Ethernet
+		// Likely this is a duplicate from a C_SET that we received and published
+		// in msg_callback.  Not much we can do to avoid the duplicate as there is 
+		// no way to tell if we performed the publish that is triggering  this callback.
+		char buf[MAXDATASIZE];
+		char buf2[MAXDATASIZE];
+		int nbytes = snprintf(buf, MAXDATASIZE, "%d;%d;%d;%d;%d;%s\n",
+				msg.sender, msg.sensor, mGetCommand(msg),
+				mGetAck(msg), msg.type, msg.getString(buf2));
+
+		pthread_mutex_lock(&controllers_sockets_mutex);
+		for (unsigned int i = 0; i < controllers_sockets.size(); i++) {
+			if (send(controllers_sockets[i], buf, nbytes, 0) == -1)
+				perror("send");
+		}
+		pthread_mutex_unlock(&controllers_sockets_mutex);
 	}	
 	else
 	{
