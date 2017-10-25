38961995ce5b89cffd905f61cd9f7e7b4a256121
Aaron <aaron832@users.noreply.github.com>
2017-10-25 14:11:40 -0700

    Working communication logic with gateway/openhab.  Updated comments.

--- AaronSensors/MusicBox/MusicBox.ino
+++ AaronSensors/MusicBox/MusicBox.ino
@@ -113,22 +113,18 @@ void loop()
 	uint8_t value;
 	static uint8_t lastValue=2;
 	
-	//We were going to sleep for input... request the status of our input.
+  //We are awake.  Now request the status of our switch from the gateway / openhab
   Serial.println("Request from server...");
-	request(CHILD_ID_RELAY1, S_LIGHT);
+	request(CHILD_ID_RELAY1, V_LIGHT);
 
-  //mysgw: TSF:MSG:READ,10-10-0,s=0,c=2,t=3,pt=0,l=0,sg=0:
-  //mysgw: Sending message on topic: mygateway1-out/10/0/2/0/3
+  //mysgw: TSF:MSG:READ,10-10-0,s=0,c=2,t=2,pt=0,l=0,sg=0:
+  //mysgw: Sending message on topic: mygateway1-out/10/0/2/0/2
 	
-	//Wait two seconds for a response.
-	//Might get a button event which hopefully the attached interrupt will get.
-  //This wait is how the sensor is able to receive messages.
-  //Currently, only while its in this spot can it receive a message.
-  //Currently shows error on server saying that it wasn't ack.
-  //I think I need to ask the server to send us our variable.. and then immedietly recieve it.
-  //Which might be that request above??? but it looks like its comming through on the wrong type?
-  //The server is def getting the request and responding... but we just are not receiving anything... need to look into request
-	wait(5000);
+	//Wait two seconds for a response as openhab and the gateway might need
+  //some time to talk back after our request.
+  //This wait specifically loops in mysensor logic for communication.
+  //This is where our receive function will get called and activateBox would be set.
+	wait(2000);
 	
 	//If we got the button interrupt, then play the box.
 	if(activateBox)
@@ -140,11 +136,11 @@ void loop()
   Serial.print("Sleep for ... ");
   Serial.println(SLEEP_TIME);
 	//This sleep will will cause the radio to power down.
-	//Cant do that if we actually want to be able to sent it commands.
-  //I guess this is just to look for a button press and power down for a bit...
+	//Hopefully powering everything down... with a button wakeup
 	sleep(BUTTON_PIN-2, CHANGE, SLEEP_TIME);
 	
 	//This is if we get interrupt during sleep from the button.
+  //Not sure if this works or not.  Haven't tested out sleep wakeup.
 	value = digitalRead(BUTTON_PIN);
 	if (value==HIGH) {
     //ignore button temporarily
@@ -160,11 +156,7 @@ void receive(const MyMessage &message) {
 			if(message.getInt() == 1) {
 				// Turn on the relay briefly.
 				Serial.println("We Activated!");
-
 				ActivateBox();
-        
-				//Reply with turning the value to 0 again.
-				send(msgMusicBoxSwitch1.set(0));
 			}
 		}
 	}
