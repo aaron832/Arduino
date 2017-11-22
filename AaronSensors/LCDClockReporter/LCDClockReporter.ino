c1480d13e409096a974f2d01ab8f49955e71cba7
Aaron <aaron832@users.noreply.github.com>
2017-11-21 20:47:33 -0800

    Clock now getting top and bottom text from openhab.  Added special character support.

--- AaronSensors/LCDClockReporter/LCDClockReporter.ino
+++ AaronSensors/LCDClockReporter/LCDClockReporter.ino
@@ -38,7 +38,8 @@
 #include <LiquidCrystal.h>
 #include "Time.h"
 
-#define MAX_CHARS 17
+#define MAX_CHARS 16
+#define CLOCK_CHAR_OFFSET 6
 #define LONG_WAIT 500
 unsigned long SLEEP_TIME = 1000; // Sleep time between reports (in milliseconds)
 unsigned long WAIT_MODE_TIME = 3600000;
@@ -65,6 +66,18 @@ unsigned long receivedTime = 0;
 unsigned int displayindex = 0;
 
 unsigned long lastUpdate=0, lastRequest=0;
+bool displayMsgRec[2] = {false, false};
+
+byte specialchars[8][8] = {
+  {  B00000, B01010, B11111, B11111, B01110, B00100, B00000 },
+  {  B00000, B01010, B11111, B11111, B01110, B00100, B00000 },
+  {  B00000, B01010, B11111, B11111, B01110, B00100, B00000 },
+  {  B00000, B01010, B11111, B11111, B01110, B00100, B00000 },
+  {  B00000, B01010, B11111, B11111, B01110, B00100, B00000 },
+  {  B00000, B01010, B11111, B11111, B01110, B00100, B00000 },
+  {  B00000, B01010, B11111, B11111, B01110, B00100, B00000 },
+  {  B00000, B01010, B11111, B11111, B01110, B00100, B00000 }
+};
 
 void setup()
 {
@@ -73,23 +86,34 @@ void setup()
   pinMode(greenled, OUTPUT);
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
+  for(int i=0; i<8; i++) {
+    lcd.createChar(i, specialchars[i]);
+  }
 
-  lcd.print("Starting up...");
-  lcd.setCursor ( 0, 1 );
-  lcd.print("Request Time...");
+  writeScreen("Starting..", 0);
+  char buf[MAX_CHARS];
+  strncpy(buf, " I LOVE YOU EMI ", MAX_CHARS);
+  buf[0] = buf[MAX_CHARS-1] = 0x01;
+  writeScreen(buf, 1);
 
   requestTime();
   wait(2000, C_INTERNAL, I_TIME);
+
+  request(CHILD_ID_LCDTEXT_TOP, V_TEXT);
+  wait(2000, C_SET, V_TEXT);
+
+  //request(CHILD_ID_LCDTEXT_BOTTOM, V_TEXT);
+  //wait(2000, C_SET, V_TEXT);
 }
 
 void DisplayClock()
 {
   time_t currentTime = receivedTime + (now() - timeReceivedTime);
 
-  char buf[MAX_CHARS];
-  snprintf(buf, MAX_CHARS, "     %02d:%02d:%02d ", hour(currentTime), minute(currentTime), second(currentTime));
-  writeScreen(buf, 0);
-  writeScreen("", 1);
+  char buf[CLOCK_CHAR_OFFSET+1];
+  lcd.setCursor ( 0, 0 );
+  snprintf(buf, CLOCK_CHAR_OFFSET+1, "%02d:%02d ", hour(currentTime), minute(currentTime), second(currentTime));
+  lcd.print(buf);
 }
 
 void UpdateDisplay()
@@ -118,25 +142,40 @@ void UpdateDisplay()
 
 void writeScreen(const char* string, int sensorid)
 {
-  char buf[MAX_CHARS];
-  for(int i=0; i<MAX_CHARS; i++) {
+  char buf[MAX_CHARS+1];
+
+  int lastChar = MAX_CHARS;
+  if(sensorid == 0)
+    lastChar = (MAX_CHARS - CLOCK_CHAR_OFFSET);
+
+  //Clear out buffer and put null terminator at end
+  for(int i=0; i<=MAX_CHARS; i++) {
     buf[i] = ' ';
   }
-  buf[MAX_CHARS-1] = '\0';
-  int copylength = strlen(string);
-  if(copylength > MAX_CHARS)
-    copylength = MAX_CHARS;
-  else if(copylength > 0)
-    copylength--;  // do not copy null terminator
-  strncpy(buf,string,copylength);
+  buf[lastChar] = '\0';
+
+  //Do not want to copy over the null terminator.
+  strncpy(buf,string,min(lastChar,strlen(string)));
   //Serial.print("Display: ");
   //Serial.print(sensorid);
   //Serial.print(" S: ");
   //Serial.println(string);
   
   lcd.home();
-  lcd.setCursor ( 0, sensorid );
-  lcd.print(buf);
+  if(sensorid == 0)
+    lcd.setCursor ( CLOCK_CHAR_OFFSET, sensorid );
+  else
+    lcd.setCursor ( 0, sensorid );
+    
+  //lcd.print(buf);
+  for(int i=0; i<lastChar; i++) {
+    if(buf[i] < 9 && buf[i] > 0) {
+      lcd.write(byte(buf[i]-1));
+    }
+    else {
+      lcd.write(buf[i]);
+    }
+  }
 }
 
 void presentation()
@@ -153,14 +192,27 @@ void loop()
 {
   unsigned long now = millis();
 
-  // If no time has been received yet, request it every 10 second from controller
-  // When time has been received, request update every hour
-  if ((receivedTime == 0 && (now-lastRequest) > (10UL*1000UL))
-    || (receivedTime != 0 && (now-lastRequest) > (60UL*1000UL*60UL))) {
-    // Request time from controller. 
-    Serial.println("requesting time");
-    requestTime();
-    wait(2000, C_INTERNAL, I_TIME);
+  bool receivedAllInfo = (receivedTime != 0) && 
+    displayMsgRec[CHILD_ID_LCDTEXT_TOP] && displayMsgRec[CHILD_ID_LCDTEXT_BOTTOM];
+  
+  //If we have not received information from controller in setup, then keep
+  //requesting every 10 seconds.
+  if ( (!receivedAllInfo && (now-lastRequest) > (10UL*1000UL)) ||
+       (receivedAllInfo == true && (now-lastRequest) > (60UL*1000UL*60UL)) )
+  {
+    if(receivedTime == 0) {
+      requestTime();
+      wait(2000, C_INTERNAL, I_TIME);
+    }
+    if(!displayMsgRec[CHILD_ID_LCDTEXT_TOP]) {
+      request(CHILD_ID_LCDTEXT_TOP, V_TEXT);
+      wait(2000, C_INTERNAL, V_TEXT);
+    }
+    //if(!displayMsgRec[CHILD_ID_LCDTEXT_BOTTOM]) {
+    //  request(CHILD_ID_LCDTEXT_BOTTOM, V_TEXT);
+    //  wait(2000, C_INTERNAL, V_TEXT);
+    //}
+
     lastRequest = now;
   }
 
@@ -180,6 +232,9 @@ void loop()
       noTone(buzzer);     // Stop sound...
       playSound = false;
   }
+
+  //Do not sleep.  Otherwise clock needs to be adjusted after sleep.
+  //Now does not account for sleep duration.
   
   //sleeps_without_response++;
   //if(sleeps_without_response > MAX_SLEEPS_NO_RESPONSE) {
@@ -206,6 +261,7 @@ void loop()
 
 // This is called when a message is received
 void receive(const MyMessage &message) {
+  Serial.println("Got a message");
   if (message.type == V_TEXT) {                       // Text messages only
     sleeps_without_response = 0;
     Serial.print("Sensor: "); 
@@ -214,6 +270,7 @@ void receive(const MyMessage &message) {
     Serial.println(message.getString());     // Write some debug info
     if (message.sensor == CHILD_ID_LCDTEXT_TOP ||
         message.sensor == CHILD_ID_LCDTEXT_BOTTOM) {
+      displayMsgRec[message.sensor] = true;
       writeScreen(message.getString(), message.sensor);
     }
   }
